c49993e06ace024b0868822acc792cd628b21061
gastonfriass <64560293+gastonfriass@users.noreply.github.com>
2020-07-26 02:52:39 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ var css = document.querySelector("h3");
 var color1 = document.getElementsByClassName("color1")[0];
 var color2 = document.getElementsByClassName("color2")[0];
 var body = document.getElementById("gradiente");
+var colorRandom = document.getElementsByClassName("colorRandom")[0];
+
 
 function setGradient() {
 	body.style.background = 
@@ -13,7 +15,27 @@ function setGradient() {
 
 	css.textContent = body.style.background + ";";
 }
+document.addEventListener("DOMContentLoaded", setGradient);
+
+function getRandomColor() {
+  var letters = '0123456789ABCDEF';
+  var color = '#';
+  var colorb = '#';
+  for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+    colorb += letters[Math.floor(Math.random() * 16)];
+  }
+  return body.style.background = "linear-gradient(to right, " + color + ", " + colorb  + ")";
+}
+
+
+
 
 color1.addEventListener("input", setGradient);
 
 color2.addEventListener("input", setGradient);
+
+colorRandom.addEventListener("click", getRandomColor);
+
+
+
